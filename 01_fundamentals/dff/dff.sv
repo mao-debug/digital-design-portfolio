49409d33f7a7da25651fb52279145596df35c34c
Mao <79850862+mao-debug@users.noreply.github.com>
2026-09-06 18:20:00 -0400

    Add D flip-flop module with reset functionality

--- 01_fundamentals/dff/dff.sv
+++ 01_fundamentals/dff/dff.sv
@@ -1,1 +1,17 @@
+module dff(
+    input  clk,
+    input  reset,
+    input  d,
+    output logic q
+);
 
+
+always_ff @(posedge clk or posedge reset) begin
+
+      if (reset)
+        q <= 1'b0;
+    else
+        q <= d;
+    end
+
+endmodule
