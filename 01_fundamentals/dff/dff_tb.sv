ca46adcd4ef90a6c5a651a708efa132a99dff186
Mao <79850862+mao-debug@users.noreply.github.com>
2026-09-07 17:08:22 -0400

    fix DFF RTL and add directed verification
    Added test cases for D flip-flop functionality including reset and value storage checks.

--- 01_fundamentals/dff/dff_tb.sv
+++ 01_fundamentals/dff/dff_tb.sv
@@ -24,5 +24,61 @@ module dff_tb;
         clk = ~clk;
       end
   end
+    
+//Testcases
+initial begin
+    reset = 1'b0;
+    d = 1'b1;
+    
+    #6;
+        if (q == 1'b1) begin
+            $display("PASS: Store Value on Rising Edge");
+        end
+        else begin
+            $display("FAIL: Store Value on Rising Edge");
+        end
+    
+    #2;
+    reset = 1'b1;
 
-  
+    #1;
+        if (q == 1'b0) begin
+            $display("PASS: Asynchronous Reset");
+        end
+        else begin
+            $display("FAIL: Asynchronous Reset");
+        end
+
+    #5;
+    d = 1'b0;
+    reset = 1'b0;
+    
+    #2;
+        if (q == 1'b0) begin
+            $display("PASS: Remain/Capture 0");
+        end
+        else begin
+            $display("FAIL: Remain/Capture 0");
+        end
+    #1;
+       d = 1'b1;
+
+    #1;
+        if (q == 1'b0) begin
+            $display("PASS: Store Prev Value w/o Rising Edge");
+        end
+        else begin
+            $display("FAIL: Store Prev Value w/o Rising Edge");
+        end
+    
+    #8;
+        if (q == 1'b1) begin
+            $display("PASS: Store Value on Rising Edge");
+        end
+        else begin
+            $display("FAIL: Store Value on Rising Edge");
+        end
+    $finish;
+end
+    
+end module
